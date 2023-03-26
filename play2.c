6abf83c994ac49778ac3d9b9df8d8913e782b946
adidazbot <ayenicharlesadidaz@gmail.com>
2023-03-26 23:01:42 +0000

    C pointer explanatiom please read the code

--- play2.c
+++ play2.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 int main(void)
 {
+	int n;
 	int *u;
-	//char k;
-	//we are printing the address of the variable i in the computer storage 
-	//printf("the address od variable 'i' is %p\n", &i);
-	//printf("the address of variable 'j' is %p\n", &k);
-	//return (0);
-	//on the other hand pointers are basically variable use to locate another variable address.
-	//and pointers are denoted with " * " which in a code might look liks this int *u; we are declaring u as a pointer which 8 bytes will be allocated in the memory
-	//now this code will prove that a pointer is u
-	printf(" the size of pointer on this device is : %lu\n", sizeof(u));
+
+	n = 98;
+	u = &n;
+       	// remember that " & " is use to point the address of a variable and we already declare varibale u as a pointer 
+
+	printf(" the value of the 'p' is : %p", u);// this prints the value of P
+	// while 
+	printf(" the address of the address of 'n' is : %p", &n);// this will print the address of n 
 	return (0);
+
+
+
 	// this is the output of my code 
 	// vagrant@ubuntu-focal:~$ ./play2
 // the size of pointer on this device is : 8
+// since the pointer hold address of variables lets check this out now we will declare int n as 98 and then point at n to see the output.
 }
